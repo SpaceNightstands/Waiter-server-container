6872360dc2dddd424dd9d7be8221647a2f95380a
quq <quq@magisystem.xyz>
2021-08-10 10:39:24 +0200

    Removed sqlx-cli dependency

--- build-container.sh
+++ build-container.sh
@@ -4,17 +4,12 @@ set -e
 SERVER_FOLDER="$(pwd)/waiter-server"
 
 #Compile with musl for linux
-sudo podman run --rm -it -e SQLX_OFFLINE=true -v "$SERVER_FOLDER":/home/rust/src ekidd/rust-musl-builder cargo build --release
+sudo podman run --rm -it -v "$SERVER_FOLDER":/home/rust/src -e SQLX_OFFLINE=true ekidd/rust-musl-builder cargo build --release
 
 #Start creating image from alpine linux
 CONTAINER="$(buildah from alpine)"
 
 buildah copy --add-history "$CONTAINER" "$SERVER_FOLDER/target/x86_64-unknown-linux-musl/release/Waiter" /Waiter
-buildah copy --add-history "$CONTAINER" "$SERVER_FOLDER/migrations" /migrations
-
-#Install sqlx-cli
-buildah run --add-history "$CONTAINER" apk update
-buildah run --add-history "$CONTAINER" apk upgrade
 
 #Set entrypoint
 buildah config --add-history=true --entrypoint "/Waiter" "$CONTAINER"
